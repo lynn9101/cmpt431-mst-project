6c73d04d6e9ffea7bcd4bd4a20176c8adb022622
Ethan Chou <ethanchou128@gmail.com>
2024-04-11 14:19:03 -0700

    init commit for this branch

---
+++ spanning_tree_mpi.cpp
@@ -0,0 +1,13 @@
+#include <iomanip>
+#include <iostream>
+#include <stdlib.h>
+#include <thread>
+#include <algorithm>
+
+#include "core/graph.h"
+#include "core/edge.h"
+#include "core/get_time.h"
+
+int main() {
+    return 0;
+}
